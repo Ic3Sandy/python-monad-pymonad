0d0b71ec5549ff60dffe6ecaaada2568e6ea6b77
Ic3Sandy <monkey_king2@hotmail.com>
2023-12-25 23:06:03 +0700

    🍻 Try to use IO and remove if else

--- 9_3_undone_pymonad_ultimate.py
+++ 9_3_undone_pymonad_ultimate.py
@@ -1,70 +1,79 @@
 import csv
-import os
-from typing import Final
-from pymonad.tools import curry
+
 from pymonad.either import Left, Right
+from pymonad.io import IO
+from pymonad.tools import curry
+
 
 # Function to handle file reading
 def read_csv_file(file_path):
-    with open(file_path, 'r') as csvfile:
+    try:
+        with open(file_path, "r") as csvfile:
             reader = csv.reader(csvfile)
             data = [row for row in reader]
-            return data
-    return IO(read_file)
+        return Right(data)
+    except FileNotFoundError:
+        return Left("Error: File not found")
+
 
 def remove_header(data):
-    return (
-        Right(data[1:]) 
-        if len(data) > 1 
-        else Left("Error: Unable to remove header")
-    )
+    try:
+        return Right(data[1:])
+    except IndexError:
+        return Left("Error: Unable to remove header")
+
 
 @curry(2)
 def extract_column(column_index, data):
-    return (
-        Right(data).bind(lambda rows: 
-        Right(list(map(lambda row: row[column_index], rows))))
+    return Right(data).bind(
+        lambda rows: Right(list(map(lambda row: row[column_index], rows)))
     )
+
 
 extract_score_column = extract_column(1)
 extract_name_column = extract_column(0)
 
+
 def convert_to_float(data):
-    return (
-        Right(list(map(float, data))) 
-        if data 
-        else Left("Error: Unable to convert to float")
-    )
+    try:
+        return Right(list(map(float, data)))
+    except ValueError:
+        return Left("Error: Unable to convert to float")
 
 
 def calculate_average(column_values):
-    return  (
-        Right(sum(column_values) / len(column_values)) 
-        if column_values 
-        else Left("Error: Division by zero")
-    )
+    try:
+        return Right(sum(column_values) / len(column_values))
+    except ZeroDivisionError:
+        return Left("Error: Division by zero")
 
-# Data pipeline using the Either monad and custom sequencing operator
-csv_file_path = '1example.csv'
 
-# data =  read_csv_file(csv_file_path)
+csv_file_path = "example.csv"
+names = IO(
+    read_csv_file(csv_file_path).then(remove_header).then(extract_name_column)
+).value
 
-names = (
+
+result = IO(
     read_csv_file(csv_file_path)
-    .then (remove_header)
-    .then (extract_name_column)
+    .then(remove_header)
+    .then(extract_score_column)
+    .then(convert_to_float)
+    .then(calculate_average)
+).value
+
+
+def handle_error(error):
+    print(f"Error processing data: {error}")
+
+
+def handle_success(names, result):
+    print(f"An average score of {', '.join(names[:-1])} and {names[-1]} is {result}")
+
+
+names.either(
+    handle_error,
+    lambda names: result.either(
+        lambda error: handle_error(error), lambda result: handle_success(names, result)
+    ),
 )
-
-result = (
-    read_csv_file(csv_file_path)
-    .then (remove_header)
-    .then (extract_score_column)
-    .then (convert_to_float)
-    .then (calculate_average) 
-)
-
-
-if result.is_right and names.is_right:
-    print(f"An average score of {', '.join(names.value[:-1])} and {names.value[-1]} is {result.value}")
-else:   
-    print(f"Error processing data: {result.value}")
